555eb30c8b67070a9dab75bba3ddd8e8708e0144
nitori <jukainoito2016@gmail.com>
2021-10-21 10:08:37 +0800

    fix: modify var name & remove aws

--- checkin.py
+++ checkin.py
@@ -8,24 +8,19 @@ import sys
 import threading
 import time
 import warnings
+import typing
 from urllib.parse import urlparse
 
 import cfscrape
 import requests
 from lxml import etree
 
-import boto3
-import botocore
-from boto3.dynamodb.conditions import Key
-
-from pprint import pprint
-
 warnings.filterwarnings('ignore')
 
 parser = argparse.ArgumentParser()
 
 '''
-run explame
+run example
 
 checkin.py tsdm.cookies.json -t tsdm -f 0a0000a0 -hook http://127.0.0.1
 
@@ -50,7 +45,7 @@ parser.add_argument('-hook', '--hook', help='call web hook url, post json data {
 parser.add_argument("-cf", "--cf", help="pass Cloudflare's anti-bot page", action="store_true")
 args = parser.parse_args()
 
-formhash = args.formhash
+form_hash = args.formhash
 proxy = args.proxy
 site_type = args.type
 loopTime = args.loop
@@ -65,7 +60,7 @@ PROGRAM_DIR_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 DYNAMODB_TABLE_NAME = 'CheckinCache'
 
-taskTimer = None
+taskTimer: typing.Optional[threading.Timer] = None
 
 logging.basicConfig(level=logging.INFO, format='{} - %(asctime)s \n\t %(levelname)s - %(message)s'.format(COOKIES_FILE))
 logger = logging.getLogger(__name__)
@@ -130,17 +125,18 @@ def checkin(url, data):
     return result
 
 
-def get_formhash(url):
+def get_form_hash(url):
     r = requests.get(url, headers=HEADERS, cookies=COOKIES, proxies=PROXIES, verify=False, timeout=60)
     html_etree = etree.HTML(r.text)
-    formhash = html_etree.xpath("//*[@name='formhash']/@value")
-    formhash = ''.join(formhash)
-    if len(formhash) == 0:
-        formhash = re.match('.*;formhash=(\w*).*', r.text, re.S).group(1)
-    return formhash
+    form_hash_value = html_etree.xpath("//*[@name='formhash']/@value")
+    form_hash_value = ''.join(form_hash_value)
+    if len(form_hash_value) == 0:
+        form_hash_value = re.match('.*;formhash=(\\w*).*', r.text, re.S).group(1)
+    return form_hash_value
 
 
 def read_cookies_file():
+    f = None
     try:
         temp, ext = os.path.splitext(COOKIES_FILE)
         if ext.lower() == '.json':
@@ -171,7 +167,7 @@ def do_checkin(url, data):
     if isinstance(result, dict) and 'error' in result.keys():
         if result['error'] == 'formhash':
             logger.info("formhash: {}  错误，重新获取 formhash".format(data['formhash']))
-            data['formhash'] = get_formhash(URL_LINK[site_type]['formhash'])
+            data['formhash'] = get_form_hash(URL_LINK[site_type]['formhash'])
             logger.info("当前 formhash: {}".format(data['formhash']))
             result = checkin(url, data)
     logger.info("{}".format(result))
@@ -197,10 +193,6 @@ def get_checkin_info(check_status_obj):
     if has_checkin.find('已经签到') == -1:
         return
 
-    # toDay = time.strftime("%Y%m%d")
-    # if toDay == CACHE[site_type]:
-    # 	return
-
     checkin_info_count = html_etree.xpath('count(' + checkin_info_xpath + ')')
     checkin_info_list = []
     for i in range(int(checkin_info_count)):
@@ -208,58 +200,23 @@ def get_checkin_info(check_status_obj):
         if p is not None:
             checkin_info_list.append(p)
     checkin_info = '\n'.join(checkin_info_list)
-    # logger.info('{}'.format(checkin_info))
-    # if command is not None:
-    # 	command_line = Template(command).substitute(info=checkinInfo)
-    # 	print(command_line)
-    # 	command_args = shlex.split(command_line)
-    # 	try:
-    # 		subprocess.check_call(command_args)
-    # 	except subprocess.CalledProcessError:
-    # 		traceback.print_exc()
-    # CACHE[site_type] = toDay
     checkin_date = re.findall('(\\d{4}-\\d{2}-\\d{2})', checkin_info)
-    if len(checkin_date) > 0  and False:
-        checkin_date = checkin_date[0]
-        create_cache_table()
-        cache_date = get_cache()
-        if cache_date is None:
-            put_cache(checkin_date)
-        elif cache_date == checkin_date:
-            return
-        else:
-            update_cache(checkin_date)
-
-        # data = {}
-        # if os.path.exists(CACHE_FILE_PATH):
-        #     with open(CACHE_FILE_PATH, mode='r', encoding='utf-8') as f:
-        #         try:
-        #             data = json.load(f)
-        #         except Exception:
-        #             pass
-        #         finally:
-        #             f.close()
-        # if COOKIES_FILE in data.keys() and data[COOKIES_FILE] == checkin_date:
-        #     return
-        #
+    if len(checkin_date) > 0:
         if HOOK_URL is not None:
             payload = {'msg': checkin_info}
             logger.info('Send msg: {} to {}'.format(payload, HOOK_URL))
             requests.post(HOOK_URL, json=payload)
 
-        #
-        # data[COOKIES_FILE] = checkin_date
-        # with open(CACHE_FILE_PATH, mode='w', encoding='utf-8') as f:
-        #     json.dump(data, f)
-        #     f.close()
-
 
 def use_cf(site_obj):
-    url_info = urlparse(site_obj['checkin'])
-    domain_url = url_info.scheme + '://' + url_info.netloc
-    cookie_value, user_agent = cfscrape.get_tokens(domain_url, proxies=PROXIES)
-    HEADERS['user-agent'] = user_agent
-    COOKIES.update(cookie_value)
+    try:
+        url_info = urlparse(site_obj['checkin'])
+        domain_url = url_info.scheme + '://' + url_info.netloc
+        cookie_value, user_agent = cfscrape.get_tokens(domain_url, proxies=PROXIES)
+        HEADERS['user-agent'] = user_agent
+        COOKIES.update(cookie_value)
+    finally:
+        ...
 
 
 def main():
@@ -272,12 +229,12 @@ def main():
     if IS_CF:
         use_cf(site_obj)
 
-    global formhash
-    if formhash is None:
-        formhash = get_formhash(site_obj['formhash'])
+    global form_hash
+    if form_hash is None:
+        form_hash = get_form_hash(site_obj['formhash'])
     # logger.info('Use formhash: {}'.format(formhash))
     data = site_obj['template']
-    data['formhash'] = formhash
+    data['formhash'] = form_hash
 
     if loopTime is None:
         do_checkin(site_obj['checkin'], data)
@@ -289,105 +246,6 @@ def main():
         time.sleep(5)
     if HOOK_URL is not None:
         get_checkin_info(site_obj['checkin-status'])
-
-
-def get_dynamodb():
-    return boto3.resource('dynamodb', region_name='ap-southeast-1')
-
-
-def create_cache_table(dynamodb=None):
-    if not dynamodb:
-        dynamodb = get_dynamodb()
-    try:
-        table = dynamodb.create_table(
-            TableName=DYNAMODB_TABLE_NAME,
-            KeySchema=[
-                {
-                    'AttributeName': 'type',
-                    'KeyType': 'HASH'  # Partition key
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'type',
-                    'AttributeType': 'S'
-                }
-
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            }
-        )
-        return table
-    except Exception as e:
-        pass
-
-
-def put_cache(date, dynamodb=None):
-    if not dynamodb:
-        dynamodb = get_dynamodb()
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-
-    response = table.put_item(
-        Item={
-            'type': site_type,
-            'date': date
-        }
-    )
-    return response
-
-
-def query_cache(dynamodb=None):
-    if not dynamodb:
-        dynamodb = get_dynamodb()
-
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-    response = table.query(
-        KeyConditionExpression=Key('type').eq(site_type)
-    )
-    return response['Items']
-
-
-def get_cache():
-    cache = query_cache()
-    if len(cache) > 0:
-        pprint(cache)
-        return cache[0]['date']
-    return None
-
-
-def set_cache(date):
-    put_cache(date)
-
-
-def update_cache(date, dynamodb=None):
-    if not dynamodb:
-        dynamodb = get_dynamodb()
-
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-    response = table.update_item(
-        Key={
-            'type': site_type
-        },
-        UpdateExpression="set #d=:date",
-        ExpressionAttributeValues={
-            ':date': date,
-        },
-        ExpressionAttributeNames={
-            "#d": 'date'
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    return response
-
-
-def delete_cache_table(dynamodb=None):
-    if not dynamodb:
-        dynamodb = get_dynamodb()
-
-    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-    table.delete()
 
 
 if __name__ == '__main__':
